028d4fc48845428ea57c9298313c9602bbd4f679
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-05-16 20:35:48 -0700

    fixed reset and start buttons, loads random pokes

--- index.js
+++ index.js
@@ -1,40 +1,73 @@
-$(document).ready(function() {
-    let firstCard = undefined;
-    let secondCard = undefined;
-    let cards = $(".card");    
-    let flippedCards = 0;
-    let clicks = 0;
-    $("#totalPairs").text(cards.length / 2)
+const EASY = 6/2
+const MEDIUM = 12/2
+const HARD = 20/2
 
-    let time = 100 * 1000; // 100 seconds
-    $("#totalTime").text(time / 1000);
-    $("#timer").text(time / 1000);
-    let timer = setInterval(function() {
-        time -= 1000;
-        $("#timer").text(time / 1000);
-        if (time === 0) {
-            clearInterval(timer);
-            console.log("time is up");
-            $("#game_grid").empty()
-            $("#game_grid").append(`<h1>Game Over!
-                <button onclick="location.reload()">Play Again</button>
-            </h1>`);
-        }
-    }, 1000);
+
+function getPokemon(gamemode) {
+    let pokemonURLS = []
+    let randomNums = randomList(gamemode)
+    // console.log(randomNums)
+    for (let i=0; i < gamemode; i++) {
+        console.log(randomNums[i])
+        pokemonURLS.push(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomNums[i]}.png`)
+    }
+    return pokemonURLS
+}
+
+
+function populateGameBoard(mode) {
+    let pokemons = getPokemon(mode)
+    pokemons = shuffleArray(pokemons.concat(pokemons))
+    let i = 1
+    pokemons.forEach(pokemon => {
+        $("#game_grid").append(`
+        <div class="card" id="card${i}"}>
+            <div class="front_face">
+                <img src="${pokemon}" alt="pokemon">
+            </div>
+            <div class="back_face">
+                <img src="pokeball.png" alt="pokeball">
+            </div>
+        </div>`)
+        i++;
+    })
+}
+
+function randomList(size) {
+    let randoList = new Set()
+    for (let i = 0; i < size; i++) {
+        randoList.add(parseInt(Math.floor(Math.random() * 500)) + 1)
+    }
+    return Array.from(randoList)
+}
+
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
+$(document).ready(function() {
+    $("#timeResults").hide()
 
     $("button#resetBtn").click(function() {
         console.log("reset");
         clearInterval(timer);
-        $("#game_grid").remove()
-        $("button#startBtn").show();
+        location.reload();
     });
 
     $("button#startBtn").click(function() {
         console.log("start");
-        $("#header").after(`<div id="game_grid"></div>`);
-        // $("#game_grid").append(cards);
+        $("#header").after(`<div id="game_grid"></div>`)
+        // $("#game_grid").empty()
+        populateGameBoard(EASY)
         $("button#startBtn").hide();
-        time = 100 * 1000; // 100 seconds
+        $("#timeResults").show()
+        let time = 100 * 1000; // 100 seconds
+        $("#totalTime").text(time / 1000);
+        $("#timer").text(time / 1000);
         timer = setInterval(function() {
             $("#timer").text(time / 1000);
             time -= 1000;
@@ -47,56 +80,65 @@ $(document).ready(function() {
                 </h1>`);
             }
         }, 1000);;
-    });
-    $(".card").click(function() {
-        if (firstCard && secondCard) {
-            return;
-        }
-        if (!$(this).hasClass("flipped")) {
-            $(this).toggleClass("flipped");
-        }
-        if (!firstCard) {
-            firstCard = {};
-            firstCard.img = $(this).find(".front_face img")[0];
-            firstCard.cardID = $(this).attr("id");
-            clicks++;
-            $("#numClicks").text(clicks);
-        }
-        else {
-            secondCard = {};
-            secondCard.img = $(this).find(".front_face img")[0];
-            secondCard.cardID = $(this).attr("id");
-            if (firstCard.cardID === secondCard.cardID) {
-                secondCard = undefined;
+
+        let firstCard = undefined;
+        let secondCard = undefined;
+        let cards = $(".card");    
+        let flippedCards = 0;
+        let clicks = 0;
+        $("#totalPairs").text(cards.length / 2)
+
+        $(".card").click(function() {
+            if (firstCard && secondCard) {
                 return;
             }
-            if(firstCard.img.src === secondCard.img.src) {
-                console.log("match");
-                $(`#${firstCard.cardID}`).off("click");
-                $(`#${secondCard.cardID}`).off("click");
-                firstCard = undefined;
-                secondCard = undefined;
-                flippedCards += 2;
-                $("#numPairs").text(flippedCards / 2)
-                if (flippedCards === cards.length) {
-                    setTimeout(function() {
-                        clearInterval(timer);
-                        alert("You win!");
-                    }, 1000);    
-                }
-
+            if (!$(this).hasClass("flipped")) {
+                $(this).toggleClass("flipped");
+            }
+            if (!firstCard) {
+                firstCard = {};
+                firstCard.img = $(this).find(".front_face img")[0];
+                firstCard.cardID = $(this).attr("id");
+                clicks++;
+                $("#numClicks").text(clicks);
             }
             else {
-                console.log("no match");
-                setTimeout(function() {
-                    $(`#${firstCard.cardID}`).toggleClass("flipped");
-                    $(`#${secondCard.cardID}`).toggleClass("flipped");
+                secondCard = {};
+                secondCard.img = $(this).find(".front_face img")[0];
+                secondCard.cardID = $(this).attr("id");
+                if (firstCard.cardID === secondCard.cardID) {
+                    secondCard = undefined;
+                    return;
+                }
+                if(firstCard.img.src === secondCard.img.src) {
+                    console.log("match");
+                    $(`#${firstCard.cardID}`).off("click");
+                    $(`#${secondCard.cardID}`).off("click");
                     firstCard = undefined;
                     secondCard = undefined;
-                }, 1000);
+                    flippedCards += 2;
+                    $("#numPairs").text(flippedCards / 2)
+                    if (flippedCards === cards.length) {
+                        setTimeout(function() {
+                            clearInterval(timer);
+                            alert("You win!");
+                        }, 1000);    
+                    }
+
+                }
+                else {
+                    console.log("no match");
+                    setTimeout(function() {
+                        $(`#${firstCard.cardID}`).toggleClass("flipped");
+                        $(`#${secondCard.cardID}`).toggleClass("flipped");
+                        firstCard = undefined;
+                        secondCard = undefined;
+                    }, 1000);
+                }
+                clicks++;
+                $("#numClicks").text(clicks);
             }
-            clicks++;
-            $("#numClicks").text(clicks);
-        }
+        });
     });
+
 });
